d72b8a9a460aadcd0ccf582dd9db5de0fdbf160b
Sergio Garcia <sergiogarciagasco14@gmail.com>
2023-10-15 01:09:11 +0200

    Initial commit

---
+++ src/main/java/backend/projects/competitionApp/CompetitionAppApplication.java
@@ -0,0 +1,13 @@
+package backend.projects.competitionApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompetitionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompetitionAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/projects/competitionApp/CompetitionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.projects.competitionApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompetitionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
