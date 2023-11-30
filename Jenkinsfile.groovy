226a87082d166391fb0782f5ad63e6fa53430882
Sergio Garcia <sergiogarciagasco14@gmail.com>
2023-11-30 16:42:24 +0100

    updated Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -9,5 +9,15 @@ pipeline {
                 sh 'mvn -B -DskipTests clean package'
             }
         }
+        stage('Test') {
+            steps {
+                sh 'echo "Falta por preparar para ejecutar los tests'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                sh 'java -jar target/competitionApp-0.0.1-SNAPSHOT.jar'
+            }
+        }
     }
 }
