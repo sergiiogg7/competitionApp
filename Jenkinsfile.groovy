7c0d0d5a38eb2e336fd1a93c4ae2539f5f5010a2
Sergio Garcia <sergiogarciagasco14@gmail.com>
2023-11-30 16:43:11 +0100

    updated Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -11,7 +11,7 @@ pipeline {
         }
         stage('Test') {
             steps {
-                sh 'echo "Falta por preparar para ejecutar los tests'
+                sh 'echo "Falta por preparar para ejecutar los tests"'
             }
         }
         stage('Deploy') {
