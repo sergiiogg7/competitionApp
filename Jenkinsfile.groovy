35b0d6538e0c61a98eff172dfb7052831b008ab4
Sergio Garcia <sergiogarciagasco14@gmail.com>
2023-11-30 15:12:18 +0100

    add Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,9 +1,7 @@
 pipeline {
-    agent {
-        docker {
-            image 'maven:3.9.5-eclipse-temurin-17-alpine'
-            args '-v /root/.m2:/root/.m2'
-        }
+    agent any
+    tools {
+        maven 'mavenjenkins'
     }
     stages {
         stage('Build') {
