5c52027717a1f7ce1d7dc41d0575164a52e040d4
Sergio Garcia <sergiogarciagasco14@gmail.com>
2023-11-30 16:27:28 +0100

    updated application.properties and add Jenkinsfile

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,18 @@
+pipeline {
+    agent any
+    tools {
+        maven 'maven jenkins'
+    }
+    stages {
+        stage('Build') {
+            steps {
+                sh 'mvn -B -DskipTests clean package'
+            }
+        }
+        stage('Test') {
+            steps {
+                sh 'mvn test'
+            }
+        }
+    }
+}
