6f5215cd7834eb1c270eb2876dd06247a2bad1f1
Sergio Garcia <sergiogarciagasco14@gmail.com>
2023-11-30 15:18:18 +0100

    updated Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -9,5 +9,10 @@ pipeline {
                 sh 'mvn -B -DskipTests clean package'
             }
         }
+        stage('Test') {
+            steps {
+                sh 'mvn test'
+            }
+        }
     }
 }
