ede717b39507501c398f1e095e1d5b505282535a
Sergio Garcia <sergiogarciagasco14@gmail.com>
2024-01-09 13:19:31 +0100

    add name and surname  attributes to User object

--- src/main/java/backend/projects/competitionApp/entity/User.java
+++ src/main/java/backend/projects/competitionApp/entity/User.java
@@ -20,6 +20,10 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String surnames;
+    @Column(nullable = false)
     private String email;
     @Column(nullable = false)
     private String password;
