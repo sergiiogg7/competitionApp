1e6e1848ba1c06def5d5cc17cef5421ec47417a2
Sergio Garcia <sergiogarciagasco14@gmail.com>
2024-01-09 15:12:46 +0100

    refactor updateUser and deleteUser

--- src/main/java/backend/projects/competitionApp/controller/AuthController.java
+++ src/main/java/backend/projects/competitionApp/controller/AuthController.java
@@ -52,45 +52,4 @@ public class AuthController {
         }
         return response;
     }
-
-    @PutMapping("/user/{user_id}")
-    @Operation(summary = "Update User", description = "")
-    public ResponseEntity<User> updateUserById(@RequestBody User user, @PathVariable("user_id") Long id) {
-        boolean userAuthorized = false;
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.isAuthenticated()) {
-            String username = authentication.getName();
-            User existingUser = this.userService.getUserById(id);
-            if (username.equals(existingUser.getEmail())) { userAuthorized = true; }
-        }
-
-        if (userAuthorized) {
-            String hashedPwd = this.passwordEncoder.encode((user.getPassword()));
-            user.setPassword(hashedPwd);
-            User updatedUser = this.userService.updateUserById(user, id);
-            return new ResponseEntity<User>(updatedUser, HttpStatus.OK);
-        } else {
-            throw new UnauthorizedActionException();
-        }
-    }
-
-    @DeleteMapping("/user/{user_id}")
-    @Operation(summary = "Delete User", description = "")
-    public ResponseEntity<String> deleteUserById(@PathVariable("user_id") Long id) {
-        boolean userAuthorized = false;
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        User user = this.userService.getUserById(id);
-
-        if (authentication != null && authentication.isAuthenticated()) {
-            String username = authentication.getName();
-            if (username.equals(user.getEmail())) { userAuthorized = true; }
-        }
-
-        if (userAuthorized) {
-            this.userService.deleteUserById(id);
-            return new ResponseEntity<String>("User with id " + id + " has been deleted succesfully.", HttpStatus.OK);
-        }else {
-            throw new UnauthorizedActionException();
-        }
-    }
 }
--- src/main/java/backend/projects/competitionApp/controller/UserController.java
+++ src/main/java/backend/projects/competitionApp/controller/UserController.java
@@ -1,6 +1,8 @@
 package backend.projects.competitionApp.controller;
 
 import backend.projects.competitionApp.entity.RoomRequest;
+import backend.projects.competitionApp.entity.User;
+import backend.projects.competitionApp.exception.UnauthorizedActionException;
 import backend.projects.competitionApp.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -8,6 +10,9 @@ import jakarta.websocket.server.PathParam;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,6 +25,49 @@ import java.util.List;
 public class UserController {
 
     private UserService userService;
+
+    private PasswordEncoder passwordEncoder;
+
+    @PutMapping("/{user_id}")
+    @Operation(summary = "Update User", description = "")
+    public ResponseEntity<User> updateUserById(@RequestBody User user, @PathVariable("user_id") Long id) {
+        boolean userAuthorized = false;
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()) {
+            String username = authentication.getName();
+            User existingUser = this.userService.getUserById(id);
+            if (username.equals(existingUser.getEmail())) { userAuthorized = true; }
+        }
+
+        if (userAuthorized) {
+            String hashedPwd = this.passwordEncoder.encode((user.getPassword()));
+            user.setPassword(hashedPwd);
+            User updatedUser = this.userService.updateUserById(user, id);
+            return new ResponseEntity<User>(updatedUser, HttpStatus.OK);
+        } else {
+            throw new UnauthorizedActionException();
+        }
+    }
+
+    @DeleteMapping("/{user_id}")
+    @Operation(summary = "Delete User", description = "")
+    public ResponseEntity<String> deleteUserById(@PathVariable("user_id") Long id) {
+        boolean userAuthorized = false;
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = this.userService.getUserById(id);
+
+        if (authentication != null && authentication.isAuthenticated()) {
+            String username = authentication.getName();
+            if (username.equals(user.getEmail())) { userAuthorized = true; }
+        }
+
+        if (userAuthorized) {
+            this.userService.deleteUserById(id);
+            return new ResponseEntity<String>("User with id " + id + " has been deleted succesfully.", HttpStatus.OK);
+        }else {
+            throw new UnauthorizedActionException();
+        }
+    }
 
     //Obtener todas las requests de usuarios a las rooms en las que el dueño es user_id
     @GetMapping("/{user_id}/room/requests")
