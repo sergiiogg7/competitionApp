4e607c455331cf331bd4019956a9283207143a4d
Sergio Garcia <sergiogarciagasco14@gmail.com>
2024-01-11 09:23:48 +0100

    remove dataPlayer method on DataPlayerServiceImpl

--- src/main/java/backend/projects/competitionApp/service/impl/DataPlayerServiceImpl.java
+++ src/main/java/backend/projects/competitionApp/service/impl/DataPlayerServiceImpl.java
@@ -6,9 +6,11 @@ import backend.projects.competitionApp.entity.User;
 import backend.projects.competitionApp.exception.ResourceNotFoundException;
 import backend.projects.competitionApp.repository.DataPlayerRepository;
 import backend.projects.competitionApp.service.DataPlayerService;
+import backend.projects.competitionApp.service.RoomRequestService;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -17,6 +19,9 @@ import java.util.Optional;
 public class DataPlayerServiceImpl implements DataPlayerService {
 
     private DataPlayerRepository dataPlayerRepository;
+
+    private RoomRequestService roomRequestService;
+
     @Override
     public DataPlayer save(DataPlayer dataPlayer) {
         DataPlayer savedDataPlayer = this.dataPlayerRepository.save(dataPlayer);
@@ -26,13 +31,17 @@ public class DataPlayerServiceImpl implements DataPlayerService {
     @Override
     public DataPlayer findDataPlayerByUserAndRoom(User user, Room room) {
         DataPlayer dataPlayer = this.dataPlayerRepository.findDataPlayerByUserAndRoom(user, room).orElseThrow(
-                () -> new ResourceNotFoundException("DataPlayer", "User, Room", user.toString()+ " , " + room.toString())
+                () -> new ResourceNotFoundException("DataPlayer", "User, Room", user.getName()+ " , " + room.getName())
         );
         return dataPlayer;
     }
 
     @Override
     public void remove(DataPlayer dataPlayer) {
+        this.dataPlayerRepository.findById(dataPlayer.getId()).orElseThrow(
+                () -> new ResourceNotFoundException("DataPlayer", "id", dataPlayer.getId()+"")
+        );
+
         this.dataPlayerRepository.delete(dataPlayer);
     }
 }
