c2a479dff45dc787bede02e1ba6d89afaf5da46b
Sergio Garcia <sergiogarciagasco14@gmail.com>
2024-01-28 22:14:41 +0100

    add frontend domain to CORS

--- src/main/java/backend/projects/competitionApp/config/ProjectSecurityConfig.java
+++ src/main/java/backend/projects/competitionApp/config/ProjectSecurityConfig.java
@@ -44,7 +44,7 @@ public class ProjectSecurityConfig {
                         @Override
                         public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                             CorsConfiguration config = new CorsConfiguration();
-                            config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                            config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
                             config.setAllowedMethods(Collections.singletonList("*"));
                             config.setAllowCredentials(true);
                             config.setAllowedHeaders(Collections.singletonList("*"));
